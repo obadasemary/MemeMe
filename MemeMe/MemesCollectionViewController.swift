585a321c3269e82ac1b5afce39b95a5ea53327ae
Abdelrahman Mohamed <obada.semary@gmail.com>
2015-12-30 14:45:54 +0300

    1.7
    Add Collection View Controller

--- MemeMe/MemesCollectionViewController.swift
+++ MemeMe/MemesCollectionViewController.swift
@@ -14,4 +14,31 @@ class MemesCollectionViewController: UICollectionViewController {
     var memes: [Meme] {
         return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView?.reloadData()
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return memes.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MemeCellCollection", forIndexPath: indexPath) as! MemesCollectionViewCell
+        let row = memes[indexPath.row]
+        
+        cell.imageCell?.image = row.memedImage
+        
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let detailController = storyboard!.instantiateViewControllerWithIdentifier("MemeDetailViewController") as! MemeDetailViewController
+        detailController.meme = memes[indexPath.row]
+        navigationController!.pushViewController(detailController, animated: true)
+    }
 }
